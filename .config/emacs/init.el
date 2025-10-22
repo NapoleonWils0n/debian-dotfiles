b210d2d9f2ed73547f64bd930111dfdc29b0ef52
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-10-22 23:41:31 +0100

    emacs gptel

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1371,6 +1371,9 @@
         (replace-match "#+begin_src sh"))))
 
 
+;; end of gptel use-package config
+(add-hook 'gptel-post-response-functions #'my/gptel-fix-src-header)) 
+
 ;; ----------------------------------------------------------------------------------
 ;; garbage collection
 ;; ----------------------------------------------------------------------------------
